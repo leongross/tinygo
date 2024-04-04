344dba8df7abc35fe1eac22b5b5f7b37c3c70106
leongross <leon.gross@9elements.com>
2024-04-04 13:57:35 +0200

    add os/user.Lookup
    Signed-off-by: leongross <leon.gross@9elements.com>

--- src/os/user/user.go
+++ src/os/user/user.go
@@ -31,11 +31,27 @@ type User struct {
 	HomeDir string
 }
 
+// UnknownUserError is returned by Lookup when a user cannot be found.
+type UnknownUserError string
+
+func (e UnknownUserError) Error() string {
+	return "user: unknown user " + string(e)
+}
+
 // Current returns the current user.
 //
 // The first call will cache the current user information.
 // Subsequent calls will return the cached value and will not reflect
 // changes to the current user.
+// TODO: implement syscall.Getuid() and syscall.Getgid() to get the current user.
 func Current() (*User, error) {
 	return nil, errors.New("user: Current not implemented")
+}
+
+// Lookup looks up a user by username.
+//
+// If the user cannot be found, the returned error is of type UnknownUserError.
+// NOTE: This implementation does not support caching as the golang implementation does.
+func Lookup(username string) (*User, error) {
+	return lookupUser(username)
 }
